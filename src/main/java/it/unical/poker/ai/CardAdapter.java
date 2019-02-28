7c796fd65f2af3aedf087119b671f1d840f46887
antonio <ai6chr+github@gmail.com>
2019-02-28 15:21:19 +0100

    ffdiuh

---
+++ src/main/java/it/unical/poker/ai/CardAdapter.java
@@ -0,0 +1,27 @@
+package it.unical.poker.ai;
+
+public class CardAdapter {
+	String suit; 
+	int value; 
+	
+	CardAdapter() { 
+		
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+}
