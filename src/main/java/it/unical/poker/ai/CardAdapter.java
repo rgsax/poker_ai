afcee31b618e08bb0d5e29a1186e771817752932
antonio <ai6chr+github@gmail.com>
2019-02-28 15:46:22 +0100

    a'fiuh

--- src/main/java/it/unical/poker/ai/CardAdapter.java
+++ src/main/java/it/unical/poker/ai/CardAdapter.java
@@ -1,8 +1,70 @@
 package it.unical.poker.ai;
 
+import it.unical.mat.embasp.languages.Id;
+import it.unical.mat.embasp.languages.IllegalAnnotationException;
+import it.unical.mat.embasp.languages.ObjectNotValidException;
+import it.unical.mat.embasp.languages.Param;
+import it.unical.mat.embasp.languages.asp.ASPMapper;
+import phe.Card;
+
+
+@Id("")
 public class CardAdapter {
-	String suit; 
+	static {
+		try {
+			ASPMapper.getInstance().registerClass(CardAdapter.class);
+		} catch (ObjectNotValidException | IllegalAnnotationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	@Param(0)
 	int value; 
+	
+	@Param(1)
+	String suit; 
+	
+	// Card -> Atom 
+	CardAdapter(Card c) {
+		int cardSuit = c.getSuit(); 
+		int cardValue = c.getRank();
+		
+		switch (cardSuit) {
+		case Card.HEARTS:
+			suit = "HEARTS"; 
+			break;
+		case Card.CLUBS:
+			suit = "CLUBS"; 
+			break; 
+		case Card.SPADES:
+			suit = "SPADES"; 
+			break;
+		case Card.DIAMONDS:
+			suit = "DIAMONDS"; 
+			break; 
+		}
+		
+		value = cardValue += 2; 
+	}
+	
+	// Atom -> Card 
+	public Card getCard() {
+		int cardSuit = 0; 
+		if (suit.equals("HEARTS")) {
+			cardSuit = Card.HEARTS;
+		} else if (suit.equals("DIAMONDS")) {
+			cardSuit = Card.DIAMONDS; 
+		} else if (suit.equals("CLUBS")) {
+			cardSuit = Card.CLUBS; 
+		} else if (suit.equals("SPADES")) {
+			cardSuit = Card.SPADES; 
+		}
+		
+		int cardValue = value -= 2; 
+		
+		return new Card(cardValue, cardSuit); 
+	}
 	
 	CardAdapter() { 
 		
@@ -23,5 +85,4 @@ public class CardAdapter {
 	public void setValue(int value) {
 		this.value = value;
 	}
-	
 }
